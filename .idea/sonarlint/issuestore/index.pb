af7bd62b9ef13597790cd9c013bb718bb6031fde
alcarazaina <alcarazdainara@gmail.com>
2024-05-15 18:08:58 +0200

    update

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,14 +5,8 @@ N
 src/main/java/module-info.java,1\c\1c89a5491fbfda3fb050196d6a239f20200296e7
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
-Å
-Q.idea/sonarlint/securityhotspotstore/e/7/e76b0d50d2e605adea760ab74e9ef0a124bdcb2f,b\e\be7274a07dcead7335d28ee628e8997e05ba4d59
-w
-G.idea/sonarlint/issuestore/9/a/9ab43208551370f2424ed5f2268a6a8f976a13c6,9\3\93726ea239616860018662cf005bd849e8541e9e
 S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
-Å
-Q.idea/sonarlint/securityhotspotstore/9/a/9ab43208551370f2424ed5f2268a6a8f976a13c6,4\1\41073667adc3c0ea575f6ca776b0cc9829f72364
 q
 Asrc/main/java/com/aulafx/prueba1/controlador/SumarController.java,a\3\a3b9ab967564e8dcfb10049d333087d74c9209e9
 e
--- src/main/java/com/aulafx/prueba1/controlador/SumarController.java
+++ src/main/java/com/aulafx/prueba1/controlador/SumarController.java
@@ -1,14 +1,28 @@
 package com.aulafx.prueba1.controlador;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SumarController {
-    @FXML
-    private Label welcomeText;
 
+    // Declarar los elementos de la vista que van a jugar un papel en la aplicación
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Hello world!");
+    private Label lblResultado;
+    @FXML
+    private TextField txtOperador;
+    @FXML
+    private TextField txtOperador2;
+    @FXML
+    private Button btnSumar;
+    @FXML
+    protected void sumar(ActionEvent action){
+        int operador = Integer.parseInt(txtOperador.getText());
+        int operador2 = Integer.parseInt(txtOperador2.getText());
+        int suma = operador + operador2;
+        // quiero que esa suma la muestre la etiqueta resultado
+        lblResultado.setText(String.valueOf(suma));
     }
 }
